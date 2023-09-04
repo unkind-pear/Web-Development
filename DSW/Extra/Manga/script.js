57982e719574269def88d7573d9615c9858be953
unkind_pear <andrewbmoreira1@gmail.com>
2023-09-04 20:16:03 -0300

    Various Updates
    They are from along time ago, I don't know what they are about

---
+++ DSW/Extra/Manga/script.js
@@ -0,0 +1,10 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("h1").innerHTML = scrolled + "%";
+
+  if (scrolled == 95)
+}
